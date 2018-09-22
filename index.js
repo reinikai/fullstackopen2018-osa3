42289bacf9a7788d444f8e4ecd74feb677ba9cc0
Jani Reinikainen <jani@noviaria.com>
2018-09-22 22:01:11 +0300

    Implement 3.4 puhelinluettelon backend osa 4

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
 app.get('/info', (req, res) => {
     const lkm = 'Luettelossa on ' + persons.length + ' henkilön tiedot.'
     const pvm = new Date()
