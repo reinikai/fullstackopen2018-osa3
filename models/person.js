04fa1d268341aea3b3f70d2cd595074185e1885d
Jani Reinikainen <jani@noviaria.com>
2018-09-24 15:16:35 +0300

    Implement 3.21* eriytetty sovelluskehitys- ja tuotantotietokanta

--- models/person.js
+++ models/person.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose')
+
+if ( process.env.NODE_ENV !== 'production' ) {
+    require('dotenv').config()
+}
+
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url, { useNewUrlParser: true })
